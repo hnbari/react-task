cc5f60902000677c2079636e6fa075ae403f1e42
bari hanane <hn_bari@hotmail.com>
2022-10-04 14:09:46 +0100

    test

---
+++ src/components/Container/Container.js
@@ -0,0 +1,18 @@
+import './Container.css'
+
+const Container = () => {
+     return(
+        // <>
+        //     <h1>Hi from Container</h1>
+        
+        // </>
+
+ 
+        <div>Hi from Container</div>
+    
+   
+        
+     )
+}
+
+export default Container
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import './Footer.css'
+
+const Footer = () => {
+   return (
+
+
+      <div> <p> Hi from Footer </p></div >
+   )
+}
+
+export default Footer
